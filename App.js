928af64980f5f543500abeefc3c503fdd74e104b
Rashneema <79697800+Rashneema@users.noreply.github.com>
2024-03-04 17:03:00 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+//CLASS 181
+/*
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './screens/main'
@@ -7,5 +9,35 @@ export default function App() {
    <Main/>
   );
 }
+*/
+
+//CLASS 185
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import Main from './screens/main';
+
+const Stack = createStackNavigator();
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName='Home'
+				screenOptions={{
+					headerShown: false,
+				}}>
+				<Stack.Screen name='Home' component={Home} />
+				<Stack.Screen name='Main' component={Main} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default App;
+
 
 
